af64e90d9093619fe02ab5ebec5599766aa3d6fe
irma leandre <irmaleandre@irmas-Air.PK5001Z>
2021-05-08 18:16:59 -0400

    component cleaning complete

--- client/src/components/shared/Footer/Footer.jsx
+++ client/src/components/shared/Footer/Footer.jsx
@@ -6,16 +6,11 @@ const Footer = () => {
   return (
     <div className="footer">
       <div className="footer-links">
-        {/* <h5 className="footer-h5">Created by:</h5> */}
-        <a
-          className="footer-link"
-          href="https://www.linkedin.com/in/andri-rafti-770b49126/"
-          target="blank"
-        >
+        <p className="footer-link">
           {" "}
           Andri Rafti
           <i id="icon" className="fa fa-linkedin" aria-hidden="true"></i>
-        </a>
+        </p>
         <a
           className="footer-link"
           href="https://www.linkedin.com/in/irma-leandre-660313154/"
@@ -25,57 +20,19 @@ const Footer = () => {
           Irma Leandre
           <i id="icon" className="fa fa-linkedin" aria-hidden="true"></i>
         </a>
-        <a
-          className="footer-link"
-          href="https://www.linkedin.com/in/jordanmontero96/"
-          target="blank"
-        >
+        <p className="footer-link">
           {" "}
           Jordan Montero
           <i id="icon" className="fa fa-linkedin" aria-hidden="true"></i>
-        </a>
-        <a
-          className="footer-link"
-          href="https://www.linkedin.com/in/nyheimhunter/"
-          target="blank"
-        >
+        </p>
+        <p className="footer-link">
           {" "}
           Nyheim Hunter
           <i id="icon" className="fa fa-linkedin" aria-hidden="true"></i>
-        </a>
+        </p>
       </div>
     </div>
   );
 };
 
 export default Footer;
-
-// <MDBFooter className="footer">
-// <MDBContainer fluid className="text-center text-md-left">
-//   <MDBRow>
-//     <MDBCol md="6">
-//         <ul>
-//         <li className="list-unstyled">
-//           <a href="https://github.com/Nyheimh">Github</a>
-//             {/* <a href="https://github.com/Nyheimh" className="icons-sm fb-ic ml-1"> */}
-//             {/* <MDBIcon fab icon="github" /></a> */}
-//             </li>
-//         <li className="list-unstyled">
-//           <a href="#!">Irma Github</a>
-//         </li>
-//         <li className="list-unstyled">
-//           <a href="#!">Andri Github</a>
-//         </li>
-//         <li className="list-unstyled">
-//           <a href="https://github.com/Jordanm96">Jordan Github</a>
-//         </li>
-//       </ul>
-//     </MDBCol>
-//   </MDBRow>
-// </MDBContainer>
-// <div className="footer-copyright text-center py-3">
-//   <MDBContainer fluid>
-//     &copy; {new Date().getFullYear()} Copyright: <a href="https://github.com/Jordanm96/ima-miru"> Ima Miru </a>
-//   </MDBContainer>
-// </div>
-// </MDBFooter>
